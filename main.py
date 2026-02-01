f1be0409336dcb0d4a9c6bb807d4d95ebad713d3
Denis <denis.triebel@gmail.com>
2026-02-01 17:02:49 +0100

    final release 1.0

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ clock = pygame.time.Clock()
 running = True
 
 dt = 0
-speed = 500
+speed = 300
+score = 0
+tcoord = []
+dir = "up"
 
 
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 apple_coord = random_coord(screen_size)
+ppos = (int(player_pos.x), int(player_pos.y))
 
 
 while running:
@@ -27,34 +31,56 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("grey")
     font = pygame.font.Font(None, 32)
-    ppos = font.render(f"x: {int(player_pos.x)} y: {int(player_pos.y)}", True, (10,10,10))
+    ppos_info = font.render(f"x: {int(player_pos.x)} y: {int(player_pos.y)} score: {score}", True, (10,10,10))
     #write texts
-    screen.blit(ppos, (10,10))  
+    screen.blit(ppos_info, (10,10))  
     #draw objects
-    pygame.draw.rect(screen, "red", (player_pos.x, player_pos.y,10,10))
+    
+    head = pygame.Rect(0,0,10,10)
+    head.center = ppos
+    pygame.draw.rect(screen, "red", head)
+    
+    for i in range(score):
+        tail = pygame.Rect(0,0,10,10)
+        tail.center = tcoord[i]
+        pygame.draw.rect(screen, "green", tail)
+    
     pygame.draw.circle(screen, "blue", apple_coord, 5)
     
     #movement player
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+     
+    if keys[pygame.K_w] and dir != "down" and ppos[0] != mem_pos[0]:
         dir = "up"
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and dir != "up" and ppos[0] != mem_pos[0]:
         dir = "down"
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and dir != "rigt" and ppos[1] != mem_pos[1]:
         dir = "left"
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] and dir != "left" and ppos[1] != mem_pos[1]:
         dir = "right"
+    mem_pos = ppos
     player_pos = move_step(dir,player_pos, (screen.get_height(), screen.get_height()), speed, dt)
     
+    #tail mem
+    ppos = (player_pos.x, player_pos.y)
+    if ppos not in tcoord:
+        tcoord.append(ppos)
+    if len(tcoord) > score+1:
+        tcoord.pop(0)
+    
     #eat/respawn apple
-    
-    
-    
-   
-   
-   
-   
+    if ppos == apple_coord :
+        score +=1        
+        apple_coord = random_coord(screen_size)
+        while apple_coord in tcoord:
+            apple_coord = random_coord(screen_size) #dont spawn inside tail
+        
+    #detect collision, game over
+    if ppos in tcoord and tcoord.index(ppos) != len(tcoord)-1:
+        break
+        
+
     pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(30) / 1000
 
 pygame.quit()
--- tools.py
+++ tools.py
@@ -3,28 +3,28 @@ def move_step(dir : str, ppos, area, speed, dt):
 
     if dir == "up":
         if ppos.y <= area[1]:
-            ppos.y -= speed*dt
+            ppos.y -= round(speed*dt, -1)
         if ppos.y < 0:
             ppos.y = area[1]
         if ppos.y > area[1]:
             ppos.y = 0
     if dir == "down":
         if ppos.y <= area[1]:
-            ppos.y += speed*dt
+            ppos.y += round(speed*dt, -1)
         if ppos.y < 0:
             ppos.y = area[1]
         if ppos.y > area[1]:
             ppos.y = 0
     if dir == "left":
         if ppos.x <= area[0]:
-            ppos.x -= speed*dt
+            ppos.x -= round(speed*dt, -1)
         if ppos.x < 0:
             ppos.x = area[0]
         if ppos.x > area[0]:
             ppos.x = 0
     if dir == "right":
         if ppos.x <= area[0]:
-            ppos.x += speed*dt
+            ppos.x += round(speed*dt, -1)
         if ppos.x < 0:
             ppos.x = area[0]
         if ppos.x > area[0]:
@@ -32,4 +32,7 @@ def move_step(dir : str, ppos, area, speed, dt):
     
     return ppos
         
-        
+def random_coord(max : int):
+    import random
+    return (round(random.randint(0, max),-1), round(random.randint(0, max),-1))
+    
